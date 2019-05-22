1349c1dedeb5bc0e8573d54f65e5efa0f8d3cdcf
Alyssa Wei <wei.alyssa@gmail.com>
2019-05-22 13:07:35 -0700

    Closed db connection after seeding

--- database/data/seedDB.js
+++ database/data/seedDB.js
@@ -1,11 +1,12 @@
 var gamesData = require('./gamesData');
-var { Games } = require('../index');
+var { Games, db } = require('../index');
 
 var seed = (numDocs = 100) => {
   gamesData.getData(numDocs, (docs)=>{
     Games.insertMany(docs, function (err, results) {
       if (err) return console.error(err);
       console.log('Inserted into DB!');
+      db.close();
     });
   });
 }
