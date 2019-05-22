4e34a42ae5dd45f9549af8791995d381e9ebaf6c
Alyssa Wei <wei.alyssa@gmail.com>
2019-05-22 16:40:45 -0700

    Client GET req w/ game ID, server res with correct doc from db

--- server/index.js
+++ server/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
+var { Games } = require('../database/index');
+
 let app = express();
 const port = 3003;
 
 app.use(express.static(__dirname+'/../client/dist'));
 
-
+app.get('/games/:gameid', (req, res) => {
+  let id = req.params.gameid;
+  Games.find({ game_id: id }).limit(1).exec((err, queryResults) => {
+    res.json(queryResults[0]);
+  });
+});
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
